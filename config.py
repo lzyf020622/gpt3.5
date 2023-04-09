06c49e030fe3e40c73f4ad02799031afd51cd4c2
lzyf020622 <127021454+lzyf020622@users.noreply.github.com>
2023-04-09 16:51:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# [step 1]>> 例如： API_KEY = "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" （此key无效）
+# [step 1]>> 例如： API_KEY = "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" 
 API_KEY = "sk-此处填API密钥"
 
 # [step 2]>> 改为True应用代理，如果直接在海外服务器部署，此处不修改
